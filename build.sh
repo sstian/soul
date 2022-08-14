2f70854d0cdbe8bc65effcf29e9a9a7e5c7b4b1a
sstian <st.tian@foxmail.com>
2022-08-14 15:45:51 +0800

    deploy

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Exit immediately if a simple command exits with a non-zero status
+set -e
+
 echo "start building..."
 
 echo "git pushing changes..."
-echo ">> git add --all"           && git add --all
-echo ">> git commit -m 'deploy'"  && git commit -m "deploy"
-echo ">> git push"                && git push
+echo -e "\n>> git add --all"           && git add --all
+echo -e "\n>> git commit -m 'deploy'"  && git commit -m "deploy"
+echo -e "\n>> git push"                && git push
 
 echo "hexo deploying webpage..."
-echo ">> hexo clean"    && hexo clean
-echo ">> hexo generate" && hexo generate
-echo ">> hexo deploy"   && hexo deploy
+echo -e "\n>> hexo clean"    && hexo clean
+echo -e "\n>> hexo generate" && hexo generate
+echo -e "\n>> hexo deploy"   && hexo deploy
 
 echo "build completely"
