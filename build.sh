30b36a9ad98af2ebd4c18e31fc6902d4fec6651f
sstian <st.tian@foxmail.com>
2024-07-14 22:07:14 +0800

    enable build.sh commit message

--- build.sh
+++ build.sh
@@ -2,19 +2,26 @@
 
 # usage: e.g. bash build.sh "commit message"
 
+# exit immediately if a simple command exits with a non-zero status
+set -e
+
 # input parameters
 message=$1
 
-# exit immediately if a simple command exits with a non-zero status
-set -e
+commit_message=`echo "$message" | tr -d " "`
+if [ -z "$commit_message" ]; then 
+  commit_message="deploy"
+else 
+  commit_message="$message"
+fi
 
 echo "start building..."
 start_time=`date +%s`
 
 echo "git pushing changes..."
-echo -e "\n>> git add --all"           && git add --all
-echo -e "\n>> git commit -m 'deploy'"  && git commit -m "deploy"
-echo -e "\n>> git push"                && git push
+echo -e "\n>> git add --all"                      && git add --all
+echo -e "\n>> git commit -m \"$commit_message\""  && git commit -m "$commit_message"
+echo -e "\n>> git push"                           && git push
 
 echo "hexo deploying webpage..."
 echo -e "\n>> hexo clean"    && hexo clean
